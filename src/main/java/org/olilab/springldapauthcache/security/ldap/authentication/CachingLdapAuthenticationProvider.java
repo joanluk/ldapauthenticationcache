1aaa9ff3ba73ee78585a2b60d9db8c5956a72c64
joanluk <joanluk@gmail.com>
2018-07-01 23:17:14 +0200

    Create travis file

--- src/main/java/org/olilab/springldapauthcache/security/ldap/authentication/CachingLdapAuthenticationProvider.java
+++ src/main/java/org/olilab/springldapauthcache/security/ldap/authentication/CachingLdapAuthenticationProvider.java
@@ -13,6 +13,24 @@ import org.springframework.security.ldap.authentication.LdapAuthenticator;
 import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
 import org.springframework.util.StringUtils;
 
+/**
+ * LDAP  authentication ldap provider with cache
+ *
+ * Caching is handled by storing the <code>UserDetails</code> object being placed in the
+ * {@link UserCache}. This ensures that subsequent requests with the same username can be
+ * validated without needing to query the {@link org.springframework.security.core.userdetails.UserDetailsService}.
+ * It should be noted that if a user appears to present an incorrect password, the
+ * {@link org.springframework.security.core.userdetails.UserDetailsService}
+ * will be queried to confirm the most up-to-date password was used for comparison.
+ * Caching is only likely to be required for stateless applications. In a normal web
+ * application, for example, the <tt>SecurityContext</tt> is stored in the user's session
+ * and the user isn't reauthenticated on each request. The default cache implementation is
+ * therefore {@link NullUserCache}.
+ *
+ * @author joanluk
+ * @since 1.0.0
+ */
+
 @Slf4j
 @Setter
 public class CachingLdapAuthenticationProvider extends LdapAuthenticationProvider {
